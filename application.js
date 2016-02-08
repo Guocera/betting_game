fb3e9445bd268eeca76279eed888b0fb7a19e7df
Nick Yee <njsyee@gmail.com>
2016-02-08 14:50:54 -0700

    Moved methods to Player object.

--- application.js
+++ application.js
@@ -1,5 +1,6 @@
 function Player() {
   this.money = 100;
+
   this.broke = function broke() {
     if (this.money === 0) {
       return true;
@@ -7,30 +8,53 @@ function Player() {
       return false;
     }
   };
-}
 
-function invalidBet(bet) {
-  if (!isNaN(bet) && bet >= 5 && bet <= 10) {
-    return false;
-  } else {
-    return true;
+  this.getBet = function getBet() {
+    do {
+      this.bet = prompt("Place a bet between $5 and $10");
+    } while (invalidBet(this.bet));
+  };
+
+  this.getGuess = function getGuess() {
+    do {
+      this.guess = prompt("Guess a number between 1 and 10");
+    } while (invalidGuess(this.guess));
+  };
+
+  function invalidBet(bet) {
+    if (!isNaN(bet) && bet >= 5 && bet <= 10) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function invalidGuess(guess) {
+    if (!isNaN(guess) && guess >= 1 && guess <= 10) {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
 
-function invalidGuess(guess) {
-  if (!isNaN(guess) && guess >= 1 && guess <= 10) {
-    return false;
-  } else {
-    return true;
-  }
+
+// Game start
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var bet
-do {
-  bet = prompt("Place a bet between $5 and $10");
-} while (invalidBet(bet));
+var player = new Player()
 
-var guess
-do {
-  guess = prompt("Guess a number between 1 and 10");
-} while (invalidGuess(guess));
+player.getBet()
+player.getGuess()
+
+
+var correct = getRandomInt(1, 10)
+
+// switch guess {
+//   case (correct):
+
+// }
